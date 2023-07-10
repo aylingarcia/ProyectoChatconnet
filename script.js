2caff73a0a9a7a9c6cc043d4005f3347a46797a9
aylingarcia <aylingarcianicol@gmail.com>
2023-07-10 18:40:18 -0400

    parte inicio sesion

---
+++ script.js
@@ -0,0 +1,18 @@
+function openModal() {
+    document.getElementById("myModal").style.display = "block";
+  }
+  
+  function closeModal() {
+    document.getElementById("myModal").style.display = "none";
+  }
+  
+  function login() {
+    // Aquí puedes realizar la lógica de inicio de sesión
+    console.log("Iniciar sesión");
+  }
+  
+  function createAccount() {
+    // Aquí puedes realizar la lógica de creación de cuenta
+    console.log("Crear cuenta");
+  }
+  
